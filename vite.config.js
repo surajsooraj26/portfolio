5b4602d7b7d7499a5ba0a3b33c068e9a3440466b
Suraj <surajsooraj26@gmail.com>
2025-07-13 16:07:21 +0530

    server comment

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "0.0.0.0", // allow external access
-    port: 5173, // your Vite port
-    strictPort: true,
-    cors: true, // enable CORS if needed
-    origin: "https://a32ce37be1ad.ngrok-free.app/", // <-- Replace with your ngrok URL
-  },
+  // server: {
+  //   host: "0.0.0.0", // allow external access
+  //   port: 5173, // your Vite port
+  //   strictPort: true,
+  //   cors: true, // enable CORS if needed
+  //   origin: "https://a32ce37be1ad.ngrok-free.app/", // <-- Replace with your ngrok URL
+  // },
 });
