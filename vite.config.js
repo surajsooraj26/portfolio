8a5f334fbf586d2d1ae19e3c08f275937e3960d4
Suraj <surajsooraj26@gmail.com>
2025-07-13 16:16:56 +0530

    error correction

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/portfolio/", // Set the base path for deployment
+
   // server: {
   //   host: "0.0.0.0", // allow external access
   //   port: 5173, // your Vite port
