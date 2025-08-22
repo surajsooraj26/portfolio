74f6bdfaa1b7f53743d6a4917908cdbbeeb6ea3a
Suraj <surajsooraj26@gmail.com>
2025-08-22 12:38:50 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Navbar from "./components/Desktop/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { useMediaQuery } from "react-responsive";
 import MobileNavbar from "./components/Mobile/MobileNavbar";
 
@@ -10,13 +10,13 @@ function App() {
   const isMobile = useMediaQuery({ maxWidth: 767 });
 
   return (
-    <BrowserRouter basename="/portfolio">
+    <>
       {isMobile ? <MobileNavbar /> : <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
