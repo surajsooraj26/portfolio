d0f25f776fce1f99d43cb7382d7de731d9888767
Suraj <surajsooraj26@gmail.com>
2026-06-26 20:10:11 +0530

    mobile project 1

--- src/components/Mobile/MobileProject.jsx
+++ src/components/Mobile/MobileProject.jsx
@@ -1,8 +1,25 @@
+import projectdata from "./../../data/project.json";
+import "./MobileProject.css";
 function MobileProject() {
   return (
-    <div>
-      <h2>Mobile Project</h2>
-      <p>This is the mobile version of the project.</p>
+    <div className="mobile-projects">
+      <h1>Projects</h1>
+
+      {projectdata.map((project) => (
+        <div className="mobile-project-list">
+          <div className="mobile-project" key={project.id}>
+            <h3 className="m-p-title">{project.title}</h3>
+            <p className="section_2_para">{project.description}</p>
+            <button className="view-project-button">
+              <a href={project.link} target="_blank" rel="noopener noreferrer">
+                View Project
+              </a>
+            </button>
+          </div>
+
+          <div className="m2"></div>
+        </div>
+      ))}
     </div>
   );
 }
