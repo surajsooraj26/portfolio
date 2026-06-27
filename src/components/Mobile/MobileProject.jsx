b552092384932e045f40a0425be2b5965915b1ea
Suraj <surajsooraj26@gmail.com>
2026-06-27 12:14:26 +0530

    mobile skills

--- src/components/Mobile/MobileProject.jsx
+++ src/components/Mobile/MobileProject.jsx
@@ -1,9 +1,15 @@
 import projectdata from "./../../data/project.json";
 import "./MobileProject.css";
+import { useNavbar } from "../../context/NavbarContext";
 function MobileProject() {
+  const [showNavbar, setShowNavbar] = useNavbar();
   return (
-    <div className="mobile-projects">
+    <div className="mobile-projects" onPointerDown={() => setShowNavbar(false)}>
       <h1>Projects</h1>
+      <p className="section_2_para">
+        A selection of projects I've worked on, showcasing my skills and
+        experience.
+      </p>
 
       {projectdata.map((project) => (
         <div className="mobile-project-list">
@@ -17,7 +23,9 @@ function MobileProject() {
             </button>
           </div>
 
-          <div className="m2"></div>
+          <div className="m2">
+            <img src={project.image} alt={project.title} />
+          </div>
         </div>
       ))}
     </div>
--- src/components/Mobile/MobileSkills.jsx
+++ src/components/Mobile/MobileSkills.jsx
@@ -1,4 +1,10 @@
+import { useNavbar } from "../../context/NavbarContext";
 function MobileSkills() {
-  return <div>Mobile skills</div>;
+  const [showNavbar, setShowNavbar] = useNavbar();
+  return (
+    <div className="mobile-skills" onPointerDown={() => setShowNavbar(false)}>
+      <h1>Skills</h1>
+    </div>
+  );
 }
 export default MobileSkills;
