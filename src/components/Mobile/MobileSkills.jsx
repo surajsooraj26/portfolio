417c66c97acee8b93a00f04cc312a8cb85187d9e
Suraj <surajsooraj26@gmail.com>
2026-06-28 16:33:06 +0530

    mobile skills

--- src/components/Mobile/MobileSkills.jsx
+++ src/components/Mobile/MobileSkills.jsx
@@ -1,9 +1,31 @@
 import { useNavbar } from "../../context/NavbarContext";
+import "./MobileSkills.css";
 function MobileSkills() {
   const [showNavbar, setShowNavbar] = useNavbar();
   return (
     <div className="mobile-skills" onPointerDown={() => setShowNavbar(false)}>
       <h1>Skills</h1>
+      <div className="skills">
+        <h3>Languages</h3>
+        <ul>
+          <li>JavaScript</li>
+          <li>Python</li>
+          <li>Java</li>
+          <li>C++</li>
+        </ul>
+        <h3>Frameworks</h3>
+        <ul>
+          <li>React</li>
+          <li>Node.js</li>
+          <li>Express.js</li>
+          <li>Laravel</li>
+        </ul>
+        <h3>Databases</h3>
+        <ul>
+          <li>MySQL</li>
+          <li>Mongodb</li>
+        </ul>
+      </div>
     </div>
   );
 }
