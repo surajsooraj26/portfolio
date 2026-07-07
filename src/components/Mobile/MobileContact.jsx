2bfd050084b71a4e4d080905495c94e19da5eba1
Suraj S <surajsooraj26@gmail.com>
2026-07-07 15:33:55 +0530

    contact form

--- src/components/Mobile/MobileContact.jsx
+++ src/components/Mobile/MobileContact.jsx
@@ -1,4 +1,16 @@
+import "./MobileContact.css";
 function MobileContact() {
-  return <div className="mobile-contact"><h1>Contact</h1> </div>;
+  return <div className="mobile-contact"><h1>Contact</h1>
+        <div className="mobile-home">
+        <form action="" className="contact_form">
+          <label htmlFor="name">Name</label>
+          <input type="text" placeholder="Your Name" required />
+          <label htmlFor="email">Email</label>
+          <input type="email" placeholder="Your email" required />
+          <label htmlFor="message">Message</label>
+          <textarea placeholder="Your message" required></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </div> </div>;
 }
 export default MobileContact;
